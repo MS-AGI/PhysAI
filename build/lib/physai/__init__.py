478941d95e8f167c1360d2429b35ae5f56116437
MS-AGI <whataninfinity@gmail.com>
2025-09-25 19:54:09 +0530

    Final Commit

--- build/lib/physai/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pinn import PINN
-from .physics import *
-from .losses import *
-from .trainer import *
-from .visualization import *
-from .utils import *
--- build/lib/physai/utils.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-def to_device(tensor, device='cpu'):
-    return tensor.to(device)
